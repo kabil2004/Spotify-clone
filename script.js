9eef44e8ea22cf9e267b106bf30f862588ab4acf
kabil2004 <101283438+kabil2004@users.noreply.github.com>
2024-07-15 16:08:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function playAll(songNumber){
+    var audioElement= document.getElementById("song");
+    var playBtn=document.getElementById("play-pause");
+    var songFileName = "music/"+songNumber+".mp3";
+
+    if(audioElement.getAttribute("src")==songFileName && !audioElement.paused){
+        audioElement.pause();
+        playBtn.innerHTML='<img src="images/play-button.svg">';
+    }
+    else{
+        
+        audioElement.setAttribute("src",songFileName);
+        audioElement.play();
+        playBtn.innerHTML='<img src="images/pause-button.svg">';
+    }
+
+
+}
+
+function playaudio(){
+    var audio=document.getElementById("song");
+    var playBtn=document.getElementById("play-pause");
+    if(audio.paused){
+        audio.play();
+        playBtn.innerHTML='<img src="images/pause-button.svg">';
+    }
+    else{
+        audio.pause();
+        playBtn.innerHTML='<img src="images/play-button.svg">';
+    }
+}
